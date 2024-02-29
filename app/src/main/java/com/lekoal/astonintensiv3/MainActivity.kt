81140a8183fd7dd9ae0857844ddd30e9e5e854ca
lekoal <andlekoal@gmail.com>
2024-02-29 09:12:27 +0300

    Initial commit

---
+++ app/src/main/java/com/lekoal/astonintensiv3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lekoal.astonintensiv3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
